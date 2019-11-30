26f5717a5e6cf9e6252f76018cb7ab389ca03b58
Pablo Molina <pablomb255@gmail.com>
2019-11-30 11:52:07 +0100

    Updated site, developed new section, added photos and videos. Fixed also bug with responsive mobile menu.

--- script/indexScript.js
+++ script/indexScript.js
@@ -5,5 +5,14 @@ function showMenu() {
     } else {
         document.getElementById("phonewrapper").style.visibility="visible";
         document.getElementById("phonewrapper").style.position="initial";
+        document.getElementById("phonewrapper").style.border="1px solid black";
+        document.getElementById("phonewrapper").style.backgroundColor="#F06209";
+        document.getElementById("phonewrapper").style.fontStyle="bold";
+    }
+}
+function hideMenu() {
+    if ( window.innerWidth > 363 && document.getElementById("phonewrapper").style.visibility == "visible") {
+        document.getElementById("phonewrapper").style.visibility="hidden";
+        document.getElementById("phonewrapper").style.position="absolute";     
     }
 }
